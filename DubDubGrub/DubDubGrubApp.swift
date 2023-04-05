39ecddd7be5feaeeff2712d9949e49e717571cee
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-05 17:24:13 +0200

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Damien Gautier on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
