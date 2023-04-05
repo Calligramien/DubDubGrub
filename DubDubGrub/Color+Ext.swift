8fe95a397ff08e5682ce690b60efea392f9fb38a
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-05 17:56:45 +0200

    [ADD] Building app skeleton: adding TabView for navigation

---
+++ DubDubGrub/Color+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Ext.swift
+//  DubDubGrub
+//
+//  Created by Damien Gautier on 05/04/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
---
+++ DubDubGrub/LocationMapView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationMapView.swift
+//  DubDubGrub
+//
+//  Created by Damien Gautier on 05/04/2023.
+//
+
+import SwiftUI
+
+struct LocationMapView: View {
+    var body: some View {
+        Text("Map View")
+    }
+}
+
+struct LocationMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationMapView()
+    }
+}
