e50f93851510f417dfa087507921dd754a604aa3
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-12 18:05:51 +0200

    [ADD] Adding LocationDetailView (user interface)

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -28,15 +28,15 @@ struct LocationListView: View {
                                 .minimumScaleFactor(0.75)
                             
                             HStack {
-                                AvatarView()
+                                AvatarView(size: 35)
                                 
-                                AvatarView()
+                                AvatarView(size: 35)
                                 
-                                AvatarView()
+                                AvatarView(size: 35)
                                 
-                                AvatarView()
+                                AvatarView(size: 35)
                                 
-                                AvatarView()
+                                AvatarView(size: 35)
                             }
                         }
                         .padding(.leading)
@@ -49,11 +49,14 @@ struct LocationListView: View {
 }
 
 struct AvatarView: View {
+    
+    var size: CGFloat
+    
     var body: some View {
         Image("default-avatar")
             .resizable()
             .scaledToFit()
-            .frame(width: 35, height: 35)
+            .frame(width: size, height: size)
             .clipShape(Circle())
     }
 }
