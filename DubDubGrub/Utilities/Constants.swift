c4ee96f52d87b51c5ef40668044196f08c797b3d
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-19 16:49:05 +0200

    [ADD] Initial setup for CloudKitManager

---
+++ DubDubGrub/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DubDubGrub
+//
+//  Created by Damien Gautier on 19/04/2023.
+//
+
+import Foundation
+
+enum RecordType {
+    static let location = "DDGLocation"
+    static let profile  = "DDGProfile"
+}
